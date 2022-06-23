4868b92556707ef14b3cf409a8ca360c5e85ea7d
seongjaee <labyrinthird@gmail.com>
2022-06-23 16:29:32 +0900

    Update 외벽 점검

--- Codes/Programmers/외벽점검.py
+++ Codes/Programmers/외벽점검.py
@@ -5,6 +5,7 @@ from collections import deque
 def clock(n, perm, weak):
     result = 10
     for _ in range(len(weak)):
+        # weak 회전
         weak.append(weak.popleft() + n)
         new_weak = weak.copy()
         now = new_weak[0]
@@ -12,29 +13,6 @@ def clock(n, perm, weak):
         while new_weak and i < len(perm):
             now += perm[i]
             while new_weak and new_weak[0] <= now:
-                new_weak.popleft()
-            if new_weak:
-                now = new_weak[0]
-            i += 1
-
-        if new_weak:
-            continue
-
-        result = min(result, i)
-
-    return result
-
-
-def counter_clock(n, perm, weak):
-    result = 10
-    for _ in range(len(weak)):
-        weak.append(weak.popleft() - n)
-        new_weak = weak.copy()
-        now = new_weak[0]
-        i = 0
-        while new_weak and i < len(perm):
-            now -= perm[i]
-            while new_weak and new_weak[0] >= now:
                 new_weak.popleft()
             if new_weak:
                 now = new_weak[0]
@@ -56,7 +34,6 @@ def solution(n, weak, dist):
 
     for perm in perms:
         answer = min(answer, clock(n, perm, weak))
-        answer = min(answer, counter_clock(n, perm, reversed_weak))
 
     if answer == 10:
         return -1
