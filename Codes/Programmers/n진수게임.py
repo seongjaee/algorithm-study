002c78a595825c2fed64d8e6dcdf5ba361412ba0
seongjaee <labyrinthird@gmail.com>
2022-08-17 22:26:04 +0900

    Update n진수 게임

--- Codes/Programmers/n진수게임.py
+++ Codes/Programmers/n진수게임.py
@@ -30,3 +30,40 @@ def solution(n, t, m, p):
         num += 1
 
     return answer
+
+
+
+
+
+# 2번째 풀이
+def convert(num, n):
+    res = ''
+    while num > 0:
+        q, r = divmod(num, n)
+        if r >= 10:
+            res += chr(ord('A') + r - 10)
+        else:
+            res += str(r)
+        num = q
+    return res[::-1]
+
+def solution(n, t, m, p):
+    now_num = 0
+    now_converted_num = '0'
+    turn = 0
+    answer = ''
+    now_i = 0
+    
+    while len(answer) < t:
+        if turn == p - 1:
+            answer += now_converted_num[now_i]
+        
+        now_i += 1
+        turn = (turn + 1) % m
+        
+        if now_i >= len(now_converted_num):
+            now_num += 1
+            now_converted_num = convert(now_num, n)
+            now_i = 0
+
+    return answer
