87de80f1ba21d8dc67cf466818a6459a832dcd2e
seongjaee <labyrinthird@gmail.com>
2022-09-13 15:56:26 +0900

    Solve 23354

---
+++ Codes/BOJ/23354_군탈체포조.py
@@ -0,0 +1,73 @@
+from heapq import heappop, heappush
+import sys
+
+input = sys.stdin.readline
+INF = 1e10
+DELTA = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+
+def dijkstra(sy, sx):
+    distances = [[INF] * n for _ in range(n)]
+    distances[sy][sx] = 0
+    heap = [(0, sy, sx)]
+
+    while heap:
+        dist, y, x = heappop(heap)
+
+        for dy, dx in DELTA:
+            ny, nx = y + dy, x + dx
+            if ny < 0 or ny >= n or nx < 0 or nx >= n:
+                continue
+
+            if distances[ny][nx] <= dist + matrix[ny][nx]:
+                continue
+
+            distances[ny][nx] = dist + matrix[ny][nx]
+            heappush(heap, (dist + matrix[ny][nx], ny, nx))
+
+    return distances
+
+
+def dp(node, visited):
+    if visited == (1 << m) - 1:
+        return graph[node][0]
+
+    if cache[node][visited] != INF:
+        return cache[node][visited]
+
+    temp = INF
+    for nxt in range(m):
+        if visited & (1 << nxt):
+            continue
+        cost = graph[node][nxt] + dp(nxt, visited | (1 << nxt))
+        temp = min(temp, cost)
+
+    cache[node][visited] = temp
+    return temp
+
+
+n = int(input())
+matrix = [[*map(int, input().split())] for _ in range(n)]
+nodes = []
+for i in range(n):
+    for j in range(n):
+        if matrix[i][j] == 0:
+            nodes.append((i, j))
+
+        elif matrix[i][j] == -1:
+            nodes.append((i, j))
+            army = (i, j)
+
+matrix[army[0]][army[1]] = 0
+m = len(nodes)
+graph = [[0] * m for _ in range(m)]
+for i in range(m):
+    y, x = nodes[i]
+    distances = dijkstra(y, x)
+    for j in range(i + 1, m):
+        ny, nx = nodes[j]
+        graph[i][j] = distances[ny][nx]
+        graph[j][i] = distances[ny][nx]
+
+cache = [[INF] * (1 << m) for _ in range(m)]
+print(dp(0, 1))
