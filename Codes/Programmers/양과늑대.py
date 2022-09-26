48b433fb80371abce2c4bc03b752d036cce21f7b
seongjaee <labyrinthird@gmail.com>
2022-09-26 12:13:01 +0900

    Update 양과늑대

--- Codes/Programmers/양과늑대.py
+++ Codes/Programmers/양과늑대.py
@@ -32,3 +32,45 @@ def solution(info, edges):
     backtrack(0, {0}, 1, 1, 0)
 
     return answer
+
+
+# 두번째 풀이
+def solution(info, edges):
+    def backtrack(sheep, wolf):
+        nonlocal answer
+        if sheep <= wolf:
+            return
+        answer = max(answer, sheep)
+
+        for i in range(n):
+            if not adjacency[i] or visited[i]:
+                continue
+
+            visited[i] = True
+            adjacency[i] = False
+            for nxt in graph[i]:
+                adjacency[nxt] = True
+
+            backtrack(sheep + int(info[i] == 0), wolf + int(info[i] == 1))
+
+            visited[i] = False
+            adjacency[i] = True
+            for nxt in graph[i]:
+                adjacency[nxt] = False
+
+    n = len(info)
+    graph = [[] for _ in range(n)]
+    for p, c in edges:
+        graph[p].append(c)
+
+    visited = [False] * n
+    adjacency = [False] * n
+
+    visited[0] = True
+    for nxt in graph[0]:
+        adjacency[nxt] = True
+
+    answer = 0
+    backtrack(1, 0)
+
+    return answer
