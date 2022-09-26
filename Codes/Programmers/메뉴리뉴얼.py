48e8429606baae824b4d8ecfc7c2377a8d2e5083
seongjaee <labyrinthird@gmail.com>
2022-09-26 18:16:17 +0900

    Solve 메뉴리뉴얼

--- Codes/Programmers/메뉴리뉴얼.py
+++ Codes/Programmers/메뉴리뉴얼.py
@@ -23,3 +23,32 @@ def solution(orders, course):
     answer.sort()
 
     return answer
+
+
+# 두번째 풀이
+from itertools import combinations as cbnts
+
+
+def solution(orders, course):
+    orders = [sorted(order) for order in orders]
+    answer = []
+    for k in course:
+        menus_counter = {}
+        max_count = 2
+        max_menus = []
+        for order in orders:
+            comb = list(cbnts(order, k))
+            for menus in comb:
+                menus_counter[menus] = menus_counter.get(menus, 0) + 1
+
+                if max_count < menus_counter[menus]:
+                    max_count = menus_counter[menus]
+                    max_menus = ["".join(menus)]
+                elif max_count == menus_counter[menus]:
+                    max_menus.append("".join(menus))
+
+        answer += max_menus
+
+    answer.sort()
+
+    return answer
