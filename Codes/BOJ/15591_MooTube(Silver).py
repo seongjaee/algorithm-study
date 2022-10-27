47dc9bab7072692c0ec0a0b4a1faedf9ba604ea1
seongjaee <labyrinthird@gmail.com>
2022-10-27 21:11:54 +0900

    Update 15591

--- Codes/BOJ/15591_MooTube(Silver).py
+++ Codes/BOJ/15591_MooTube(Silver).py
@@ -41,3 +41,49 @@ for _ in range(Q):
         if usado[i][v] >= k:
             res += 1
     print(res)
+
+# 2번째 풀이
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+
+def bfs(start):
+    queue = deque([(start, 1e10)])
+    visited = [False] * (N + 1)
+    visited[start] = True
+
+    usado = [1e10] * (N + 1)
+
+    while queue:
+        node, dist = queue.popleft()
+
+        for nxt_node, nxt_cost in graph[node]:
+            if visited[nxt_node]:
+                continue
+            visited[nxt_node] = True
+            nxt_dist = min(dist, nxt_cost)
+            usado[nxt_node] = nxt_dist
+            queue.append((nxt_node, nxt_dist))
+
+    return usado
+
+
+N, Q = map(int, input().split())
+
+graph = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    p, q, r = map(int, input().split())
+    graph[p].append((q, r))
+    graph[q].append((p, r))
+
+for _ in range(Q):
+    k, v = map(int, input().split())
+    usado = bfs(v)
+    result = 0
+    for i in range(1, N + 1):
+        if usado[i] >= k:
+            result += 1
+    print(result - 1)
