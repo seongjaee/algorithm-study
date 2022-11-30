3a911145a508e733f2d25b4a8c016ff031e6d38e
seongjaee <labyrinthird@gmail.com>
2022-11-30 18:18:25 +0900

    Update 3687

--- Codes/BOJ/3687_성냥개비.py
+++ Codes/BOJ/3687_성냥개비.py
@@ -3,25 +3,30 @@ import sys
 input = sys.stdin.readline
 
 
-MATCH_NUM = {2: [1], 3: [7], 4: [4], 5: [2, 3, 5], 6: [0, 6, 9], 7: [8]}
+MATCH_NUM = {
+    2: ["1"],
+    3: ["7"],
+    4: ["4"],
+    5: ["2", "3", "5"],
+    6: ["0", "6", "9"],
+    7: ["8"],
+}
 
-memo = [["", ""], ["", ""], [1, 1], [7, 7], [4, 11]]
+memo = [("", ""), ("", ""), ("1", "1"), ("7", "7"), ("4", "11")]
 
 for i in range(5, 101):
     min_value = int(1e51)
     max_value = -1
-    for num, matches in MATCH_NUM.items():
-        if i - num == 1 or i - num < 0:
+    for count, digits in MATCH_NUM.items():
+        if i - count == 1 or i - count < 0:
             continue
-        prev_min, prev_max = memo[i - num]
+        prev_min, prev_max = memo[i - count]
         numbers = []
-        for match in matches:
-            numbers.append(f"{prev_min}{match}")
-            numbers.append(f"{prev_max}{match}")
-            if match == 0:
-                continue
-            numbers.append(f"{match}{prev_min}")
-            numbers.append(f"{match}{prev_max}")
+        for digit in digits:
+            numbers.append(prev_min + digit)
+            numbers.append(prev_max + digit)
+            numbers.append(digit + prev_min)
+            numbers.append(digit + prev_max)
 
         for str_number in numbers:
             if str_number[0] == "0":
@@ -29,7 +34,7 @@ for i in range(5, 101):
             min_value = min(min_value, int(str_number))
             max_value = max(max_value, int(str_number))
 
-    memo.append([min_value, max_value])
+    memo.append((str(min_value), str(max_value)))
 
 
 t = int(input())
