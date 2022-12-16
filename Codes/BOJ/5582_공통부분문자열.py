6c1ef1d70506683a34339a3c3f34cd9b1fa16e70
seongjaee <labyrinthird@gmail.com>
2022-12-16 23:12:01 +0900

    Update 5582

--- Codes/BOJ/5582_공통부분문자열.py
+++ Codes/BOJ/5582_공통부분문자열.py
@@ -11,3 +11,24 @@ for i in range(1, len(A) + 1):
             dp[j] = 0
 
 print(answer)
+
+
+## 두번째 풀이
+import sys
+
+input = sys.stdin.readline
+
+s1 = input().rstrip()
+s2 = input().rstrip()
+
+n = len(s1)
+m = len(s2)
+matrix = [[0] * (m + 1) for _ in range(n + 1)]
+result = 0
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if s1[i - 1] == s2[j - 1]:
+            matrix[i][j] = matrix[i - 1][j - 1] + 1
+            result = max(result, matrix[i][j])
+
+print(result)
